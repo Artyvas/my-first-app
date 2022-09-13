8734cf18c47e0ee6fbe81ee14d0b8124e0ac370f
Artem Vasilyev <artemvasily@gmail.com>
2022-09-12 18:03:57 -0700

    'changesmadepercomponentssectionofthecourse'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles:[`
+  //   h3 {
+  //     color: green;
+  //   }
+  // `]
+  // This is a way to use styles in-line instead of via StyleSheetList. It is an array since you can use multiple stylesheets
 })
 export class AppComponent {
 }
