0ffc8dc38b71a0617ffac1d39b854ecc41716764
Artem Vasilyev <artemvasily@gmail.com>
2022-09-14 15:13:29 -0700

    'makes-changes-following-course-databinding-directives'

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,16 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = true;
+  serverCreationStatus = "No server was created!";
+  serverName = "Test Server";
+  serverCreated = false
+  servers = ['Testserver', 'Testserver 2'];
 
-  constructor() { }
-
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = !this.allowNewServer;
+    },2000);
+   }
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    console.log(this.serverCreationStatus);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName =(<HTMLInputElement>event.target).value;
   }
 
 }
